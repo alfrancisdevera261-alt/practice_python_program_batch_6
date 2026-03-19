e697464b5e9a0edf78c2fcb962a6a3f73e7046f9
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 14:22:22 +0800

    created a center text equation so that the text will be centered

--- program_that_do_center().py
+++ program_that_do_center().py
@@ -5,3 +5,5 @@ total_width = len(text) + width
 left_width = total_width // 2
 right_width = total_width - left_width
 
+centered_text = " " * left_width + text + " " * right_width
+print(centered_text)
