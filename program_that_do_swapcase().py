a61d9e277ea6bd4e82a1fe6f6902118f42ce1c5f
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 14:30:40 +0800

    added a for loop that checks if the characters is in upper case then i will turn lower and if not it will be in uppercase

--- program_that_do_swapcase().py
+++ program_that_do_swapcase().py
@@ -1,1 +1,10 @@
 text = input("Enter a input: ")
+result = ""
+
+for characters in text:
+    if characters.isupper():
+        result += characters.lower()
+    else:
+        result += characters.upper()
+
+print(result)
