86e9f4457a16d9dba8512a989f89f3ae2e9fb26d
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 11:50:37 +0800

    added a for loop to check all the characters of the in the text if they are in upper case then it will swapcase if its true then added into the result

--- program_that_lower_all_the_case_of_characters.py
+++ program_that_lower_all_the_case_of_characters.py
@@ -1,1 +1,9 @@
 text = input("Enter a text: ")
+result = ""
+
+for text_case in text:
+    if text_case.isupper():
+        result += text_case.swapcase()
+    else:
+        result += text_case
+print(result)
