c50b43b16f534f110d457dedd1834faa671580c9
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 15:06:19 +0800

    added the title word list to append so that the word can join again together and added join() to added spaces between the word

--- program_that_do_tit;e().py
+++ program_that_do_tit;e().py
@@ -1,6 +1,9 @@
 text = input("Enter a text: ")
 words = text.split()
-
+title_word = []
 for word in words:
     if len(word) > 0:
-        title_word = word[0].upper + word[1:].lower
+        format_word = word[0].upper() + word[1:].lower()
+        title_word.append(format_word)
+result = " ".join(title_word)
+print(result)
