38647e1aa84671d9612ec9f4b8071f8158d04ad3
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 14:13:17 +0800

    added a uf function that check if the space is needed then it will add the space string multiply by the space needed for it

--- program_that_do_like_ljust().py
+++ program_that_do_like_ljust().py
@@ -3,3 +3,8 @@ width = int(input("Enter the width: "))
 
 space_needed = width - len(text)
 
+if space_needed > 0:
+    formatted_text = text + "-" * space_needed
+else:
+    formatted_text = text
+print(formatted_text)
