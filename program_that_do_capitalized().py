de8d6980b780f1ba893c6541c7dcef02ce86de18
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-19 14:45:00 +0800

    added result to add the first and the reset of the characters

--- program_that_do_capitalized().py
+++ program_that_do_capitalized().py
@@ -1,3 +1,5 @@
 text = input("Enter a text: ")
 first_character = text[0].upper()
-rest_of_character = text[1:].lower
+rest_of_character = text[1:].lower()
+result = first_character + rest_of_character
+print(result)
